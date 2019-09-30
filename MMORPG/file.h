dcf4df6331b7870a46081af4c87ed5bea17ba295
Uriel Guzmán <uriel@aromadigital.mx>
2019-09-30 12:01:37 -0500

    Added modify/remove/show/new submenu for modify

--- MMORPG/file.h
+++ MMORPG/file.h
@@ -10,10 +10,12 @@ using namespace std;
 
 class File {
 public:
-    void modify(string attr);
+    void modify(string &search, string attr);
+    string authenticate();
     void add();
-    void remove();
-    void show();
+    void remove(string searchName);
+    void show(string searchName);
+    bool validate(const string attr, const string search);
 private:
 };
 // Write
@@ -34,7 +36,7 @@ void read(istream& file, T& value) {
 
 // Write string
 void write_string(ofstream& file, const string& value) {
-    int size = value.size();
+    int size = int(value.size());
     file.write(reinterpret_cast<char *>(&size), sizeof(size));
     file.write(value.c_str(), size);
 }
@@ -48,61 +50,197 @@ void read_string(ifstream& file, string &attr) {
     attr = buffer;
     delete [] buffer;
 }
+bool File::validate(const string attr, const string search)
+{
+    ifstream file("characters.bin", ios::binary | ios::in);
+    if (file.fail())
+        cout << "No existe este archivo" << endl;
+    string name, gender, type, guild;
+    int exp, level;
+    while(true) {
+        read_string(file, name);
+        read_string(file, gender);
+        read_string(file, type);
+        read_string(file, guild);
+        read(file, exp);
+        read(file, level);
+
+        if (file.eof())
+            break;
+        if (attr == "name" && search == name) {
+            file.close();
+            return false;
+        }
+    }
+        file.close();
+        return true;
+}
 
 void File::add()
 {
     ofstream file("characters.bin", ios::out | ios::binary | ios::app);
     if (file.fail())
         cout << "Ocurrió un error escribiendo a este archivo" << endl;
-    string str;
-    int len;
+    string name, gender, type;
+    int exp = 0, level = 1;
 
     cout << "Nombre: ";
-    cin >> str;
-    len = str.size();
-    write_string(file, str);
-
+    cin >> name;
+    if (!validate("name", name)) {
+        cout << "Este nombre ya existe" << endl;
+        file.close();
+        return;
+    }
     cout << "Género: " ;
-    cin >> str;
-    len = str.size();
-    write_string(file, str);
+    cin >> gender;
 
     cout << "Tipo: ";
-    cin >> str;
-    len = str.size();
-    write_string(file, str);
+    cin >> type;
 
-    write_string(file, "NaN");
-    int exp = 0, level = 1;
+    write_string(file, name);
+    write_string(file, gender);
+    write_string(file, type);
+    write_string(file, "NULL");
     write(file, exp);
     write(file, level);
 
     file.close();
 
 }
-void File::modify(string attr)
+void File::modify(string &search, string attr)
 {
+    string name, gender, type, guild;
+    int exp, level;
+    ifstream file("characters.bin", ios::binary | ios::in);
+    if (file.fail())
+        cout << "No existe este archivo" << endl;
+    ofstream help("help.bin", ios::binary | ios::out | ios::app);
 
+    while(true) {
+        read_string(file, name);
+        read_string(file, gender);
+        read_string(file, type);
+        read_string(file, guild);
+        read(file, exp);
+        read(file, level);
+        if (search == name) {
+            if (attr == "name") {
+                cout << "Nombre nuevo: " << endl;
+                cin >> name;
+                if (!validate("name", name)) {
+                    cout << "Este nombre ya existe" << endl;
+                    help.close();
+                    file.close();
+                    std::remove("help.bin");
+                    return;
+                }
+                search = name;
+            } else if (attr == "gender") {
+                cout << "Género nuevo: " << endl;
+                cin >> gender;
+            } else if (attr == "type") {
+                cout << "Tipo nuevo: " << endl;
+                cin >> type;
+            } else if (attr == "guild") {
+                cout << "Gremio nuevo: " << endl;
+                cin >> guild;
+            } else if (attr == "exp") {
+                cout << "Experiencia nueva: " << endl;
+                cin >> exp;
+            } else if (attr == "level") {
+                cout << "Nivel nuevo: " << endl;
+                cin >> level;
+            }
+        }
+
+        if (file.eof())
+            break;
+        write_string(help, name);
+        write_string(help, gender);
+        write_string(help, type);
+        write_string(help, guild);
+        write(help, exp);
+        write(help, level);
+    }
+    help.close();
+    file.close();
+    std::remove("characters.bin");
+    std::rename("help.bin", "characters.bin");
+}
+
+string File::authenticate()
+{
+    string search, name, gender, type, guild;
+    int exp, level;
+    ifstream file("characters.bin", ios::binary | ios::in);
+    if (file.fail())
+        cout << "No existe este archivo" << endl;
+    cout << "Nombre a buscar: ";
+    cin >> search;
+
+    while(true) {
+        read_string(file, name);
+        read_string(file, gender);
+        read_string(file, type);
+        read_string(file, guild);
+        read(file, exp);
+        read(file, level);
+
+        if (search == name) {
+            file.close();
+            return search;
+        }
+        if (file.eof())
+            break;
+    }
+    file.close();
+    cout << "No existe este personaje" << endl;
+    return "NULL";
 }
 
 
-void File::remove()
+void File::remove(string searchName)
 {
+    string name, gender, type, guild;
+    int exp, level;
+    ifstream file("characters.bin", ios::binary | ios::in);
+    if (file.fail())
+        cout << "No existe este archivo" << endl;
+    ofstream help("help.bin", ios::binary | ios::out | ios::app);
 
+    while(true) {
+        read_string(file, name);
+        read_string(file, gender);
+        read_string(file, type);
+        read_string(file, guild);
+        read(file, exp);
+        read(file, level);
+        if (file.eof())
+            break;
+        if (searchName != name) {
+            write_string(help, name);
+            write_string(help, gender);
+            write_string(help, type);
+            write_string(help, guild);
+            write(help, exp);
+            write(help, level);
+        }
+
+    }
+    help.close();
+    file.close();
+    std::remove("characters.bin");
+    std::rename("help.bin", "characters.bin");
 }
 
-void File::show()
+void File::show(string searchName = "default")
 {
     ifstream file("characters.bin", ios::binary | ios::in);
     if (file.fail())
         cout << "No existe este archivo" << endl;
 
-    string name;
-    string gender;
-    string type;
-    string guild;
-    int exp;
-    int level;
+    string name, gender, type, guild;
+    int exp, level;
     cout << endl << "----------------------------" << endl;
     while(true) {
         read_string(file, name);
@@ -115,18 +253,21 @@ void File::show()
 
         if (file.eof())
             break;
-
-        cout << "Nombre: " << name << endl
-             << "Género: " << gender << endl
-             << "Tipo: " << type << endl
-             << "Gremio: " << guild << endl
-             << "Experiencia: " << exp << endl
-             << "Nivel: " << level << endl;
-
-        cout << "----------------------------" << endl;
+        if (searchName == "default" || searchName == name) {
+            cout << "Nombre: " << name << endl
+                 << "Género: " << gender << endl
+                 << "Tipo: " << type << endl
+                 << "Gremio: " << guild << endl
+                 << "Experiencia: " << exp << endl
+                 << "Nivel: " << level << endl;
+            cout << "----------------------------" << endl;
+        }
 
     }
     file.close();
 
 }
+
+
+
 #endif // FILE_H
--- MMORPG/menu.cpp
+++ MMORPG/menu.cpp
@@ -8,13 +8,13 @@ Menu::Menu()
 void Menu::show()
 {
     File f;
+    string name;
     int op;
     do {
-        cout << "Menú" << endl;
+        cout << "~" << endl;
         cout << "1) Agregar nuevo personaje" << endl;
         cout << "2) Acceder a personaje" << endl;
-        cout << "3) Modificar" << endl;
-        cout << "4) Mostrar todo" << endl;
+        cout << "3) Mostrar todo" << endl;
         cout << "0) Salir" << endl;
         cin >> op;
         switch(op) {
@@ -22,13 +22,87 @@ void Menu::show()
             f.add();
             break;
         case 2:
+            name = f.authenticate();
+            if (name != "NULL") {
+                int op2;
+                do {
+                    cout << "~/" << name << endl;
+                    cout << "1) Modificar" << endl;
+                    cout << "2) Eliminar personaje" << endl;
+                    cout << "3) Mostrar personaje" << endl;
+                    cout << "4) Completar misión" << endl;
+                    cout << "0) Regresar" << endl;
+                    cin >> op2;
+                    switch(op2) {
+                    case 1:
+                        int op3;
+                        do {
+                            cout << "~/" << name << "/modificar" << endl;
+                            cout << "1) Modificar nombre" << endl;
+                            cout << "2) Modificar género" << endl;
+                            cout << "3) Modificar tipo" << endl;
+                            cout << "4) Modificar gremio" << endl;
+                            cout << "5) Modificar experiencia" << endl;
+                            cout << "6) Modificar nivel" << endl;
+                            cout << "0) Regresar" << endl;
+                            cin >> op3;
+                            switch(op3) {
+                            case 1:
+                                f.modify(name, "name");
+                                op3 = 0;
+                                break;
+                            case 2:
+                                f.modify(name, "gender");
+                                op3 = 0;
+                                break;
+                            case 3:
+                                f.modify(name, "type");
+                                op3 = 0;
+                                break;
+                            case 4:
+                                f.modify(name, "guild");
+                                op3 = 0;
+                                break;
+                            case 5:
+                                f.modify(name, "exp");
+                                op3 = 0;
+                                break;
+                            case 6:
+                                f.modify(name, "level");
+                                op3 = 0;
+                                break;
+                            case 0:
+                                break;
+                            default:
+                                cout << "Selección incorrecta" << endl;
+                                break;
+                            }
+                        } while(op3);
+                        break;
+                    case 2:
+                        f.remove(name);
+                        op2 = 0;
+                        break;
+                    case 3:
+                        f.show(name);
+                        break;
+                    case 0:
+                        break;
+                    default:
+                        cout << "Selección incorrecta" << endl;
+                    }
+                } while (op2);
+            }
             break;
         case 3:
-            break;
-        case 4:
             f.show();
+            break;
+        case 0:
+            break;
+        default:
+            cout << "Selección incorrecta" << endl;
 
 
         }
-    } while(op != 0);
+    } while(op);
 }
